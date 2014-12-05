07a130a24ca4f440e8fb629b9f5eb80ac15e29c7
Cloud Zhong <cloudzhy@gmail.com>
2014-12-05 16:12:35 +0000

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+   # according to the page , return the corresponding title.
+  def full_title(page_title = '')
+    base_title = "Cloud is doing this sample"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+  
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,19 +9,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Cloud is doing this sample"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | Cloud is doing this sample"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | Cloud is doing this sample"
+  end
+  
+   test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Cloud is doing this sample"
   end
   
 end
