cd05bba0fb72974d08567ae838411e0aa37c0757
Cloud Zhong <cloudzhy@gmail.com>
2014-12-05 16:28:56 +0000

    Finish Test Full Path

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,5 +8,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get signup_path
+    #This code is meaningless, use function to test itself.
+    assert_select "title", full_title("Sign up")
   end
 end
