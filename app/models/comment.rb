c9dfe8b817decf2f904d1db5d30ba80264b87972
Cloud Zhong <cloudzhy@gmail.com>
2015-02-03 08:06:39 +0000

    comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :micropost
   belongs_to :user
   
+  default_scope -> { order(created_at: :desc) }
+    
   validates :user_id, presence: true
   validates :micropost_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:cloud)
+    @comment = comments(:one)
+    
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Comment.count' do
+      post :create, comment: { content: "Lorem ipsum" , user: @user, micropost_id: 1}
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Comment.count' do
+      delete :destroy, id: @comment
+    end
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:michael))
+    assert_no_difference 'Comment.count' do
+      delete :destroy, id: @comment
+    end
+    assert_redirected_to root_url
+  end
 end
